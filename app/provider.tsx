44db5f14555b74b6874c22bec4fb028ffef0e7ff
0xnehasingh <nehasingh2mau@gmail.com>
2024-12-07 19:38:31 +0530

    setup for Providers

---
+++ app/provider.tsx
@@ -0,0 +1,38 @@
+// app/providers.tsx
+"use client";
+
+import * as React from 'react';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import {  baseSepolia, mainnet } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider,  http } from 'wagmi';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+
+const config = getDefaultConfig({
+  appName: 'Base Quest Hub',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID!,
+  chains: [baseSepolia, mainnet],
+  transports: {
+    [mainnet.id]: http(),
+    [baseSepolia.id]: http(),
+  },
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {mounted ? children : null}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
